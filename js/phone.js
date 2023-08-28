16d1af0d8a4a1aca8b8f75b4cf1b34cabc5fc810
Saifulkhandaker <khsaiful67@gmail.com>
2023-08-28 18:16:25 +0600

    show detail modal added

--- js/phone.js
+++ js/phone.js
@@ -27,7 +27,7 @@ const displayPhones = (phones) => {
   phones.forEach((phone) => {
     // 2-- creat a div
     const phoneCard = document.createElement("div");
-    phoneCard.classList = `card bg-base-100 shadow-xl`;
+    phoneCard.classList = `card shadow-xl`;
     // 3-- set innerHTml
     phoneCard.innerHTML = `
     <figure class="img-bg"><img src=" ${phone.image} " /></figure>
@@ -36,7 +36,7 @@ const displayPhones = (phones) => {
       <p class="card-detail">There are many variations of passages of available, but the majority have suffered</p>
       <p class="price"> $999 </p>
       <div class="card-actions justify-end">
-        <button class="btn">Show Details</button>
+        <button onclick="handleShowDetail('${phone.slug}')" class="btn">Show Details</button>
       </div>
     </div>
         `;
@@ -49,7 +49,7 @@ const displayPhones = (phones) => {
 
 // handle search
 const handleSearch = () => {
-  loadingSpinner(true) //spiner function called
+  loadingSpinner(true); //spiner function called
   const searchField = document.getElementById("searchField");
   const searchText = searchField.value;
   loadPhone(searchText);
@@ -57,11 +57,44 @@ const handleSearch = () => {
 
 // loading snipper
 const loadingSpinner = (isLoading) => {
-  const spinner = document.getElementById('spinner');
-  if(isLoading){
-    spinner.classList.remove('hidden')
+  const spinner = document.getElementById("spinner");
+  if (isLoading) {
+    spinner.classList.remove("hidden");
+  } else {
+    spinner.classList.add("hidden");
   }
-  else{
-    spinner.classList.add('hidden')
-  }
-}
+};
+
+// handle show detail
+const handleShowDetail = async (id) => {
+  console.log("click", id);
+  // show single phone detail
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/phone/${id}`
+  );
+  const data = await res.json();
+  console.log(data);
+  const phone = data.data;
+  showPhoneDetails(phone);
+};
+
+// show phone details
+const showPhoneDetails = (phone) => {
+  console.log(phone);
+  const showDetailContainer = document.getElementById("show-detail-container");
+  showDetailContainer.innerHTML = `
+  <img src=" ${phone.image} " />
+  <h1 class="font-bold text-lg"> ${phone.name} </h1>
+  <p> It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+  <h4><span class="section details">Storage:</span> ${phone?.mainFeatures.storage} </h4>
+  <h4><span class="section details">Display Size:</span> ${phone?.mainFeatures?.displaySize} </h4>
+  <h4><span class="section details">Chipset:</span> ${phone?.mainFeatures?.chipSet} </h4>
+  <h4><span class="section details">Memory:</span> ${phone?.mainFeatures?.memory} </h4>
+  <h4><span class="section details">Slug:</span> ${phone?.slug} </h4>
+  <h4><span class="section details">Realese Date:</span> ${phone?.releaseDate} </h4>
+  <h4><span class="section details">Brand:</span> ${phone?.brand} </h4>
+  <h4><span class="section details">Gps:</span> ${phone?.others?.GPS} </h4>
+  `;
+  // show the modal
+  show_details_modal.showModal();
+};
